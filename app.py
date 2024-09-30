ea1b617ec3421f51555a8c7be3e255e581dab350
Rudraksh121a <141481238+Rudraksh121a@users.noreply.github.com>
2024-09-30 08:57:54 +0000

    added jobitem

--- app.py
+++ app.py
@@ -2,8 +2,41 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+
+JOBS=[
+
+    {
+        'id':1,
+        'title':"Data Analyst",
+        'location':"Bangaluru,India",
+        'salary':"RS. 1,00,000"
+    },
+
+
+    {
+        'id':2,
+        'title':"Data Engineer",
+        'location':"Bangaluru,India",
+        'salary':"RS. 1,20,000"
+    },
+      {
+        'id':3,
+        'title':"frontend Engineer",
+        'location':"Bangaluru,India",
+       
+    },
+      {
+        'id':4,
+        'title':"backend Engineer",
+        'location':"Bangaluru,India",
+        'salary':"RS. 1,20,000"
+    },
+]
+
 @app.route("/")
 def hello_web():
-    return render_template('home.html')
+    return render_template('home.html'
+                           ,jobs=JOBS,
+                           company_name="Job")
 if __name__== "__main__":
     app.run(host='0.0.0.0',debug=True)
