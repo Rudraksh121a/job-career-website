09ef31c467368f4244e4f0fb0af71b5065a48933
Rudraksh121a <141481238+Rudraksh121a@users.noreply.github.com>
2024-10-01 08:40:44 +0000

    html part done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app=Flask(__name__)
 
@@ -38,5 +38,9 @@ def hello_web():
     return render_template('home.html'
                            ,jobs=JOBS,
                            company_name="Job")
+
+@app.route("/jobs")
+def list_jobs():
+    return 
 if __name__== "__main__":
     app.run(host='0.0.0.0',debug=True)
